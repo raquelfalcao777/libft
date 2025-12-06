51803037ee23cd214f1a1d0ad7026423155a282d
raquel.falcao777 <raquenrfalcao@hotmail.com>
2025-12-06 13:13:02 +0100

    strmapi striteri

---
+++ ft_putendl_fd.c
@@ -0,0 +1,11 @@
+// 42 header
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if(!s)
+		return (0);
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -2,7 +2,9 @@
 // insert 42 header
 #include "libft.h"
 
-void	ft_putchar_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    write(fd, &s, 1);
+	if(!s)
+		return (0);
+	write(fd, s, ft_strlen(s));
 }
